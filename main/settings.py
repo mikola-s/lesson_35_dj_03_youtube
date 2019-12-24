bbc60f586820d6f8426c949c1c4fdcac5003d16c
mikola-s <suharev.nv@gmail.com>
2019-12-24 22:12:24 +0200

    lsn35 include apps in INSTALLED_APPS in main/settings.py

--- main/settings.py
+++ main/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'youtube.apps.YoutubeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
