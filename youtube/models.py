d99de810992de52a99ca2bbeca55766b78f53cfc
mikola-s <suharev.nv@gmail.com>
2019-12-24 23:02:19 +0200

    lsn35 add mock models in youtube/models

--- youtube/models.py
+++ youtube/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Gender(models.Model):
+    pass
+
+
+class Status(models.Model):
+    pass
+
+
+class User(models.Model):
+    pass
+
+
+class Channel(models.Model):
+    pass
+
+
+class Movie(models.Model):
+    pass
