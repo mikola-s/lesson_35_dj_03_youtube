7e94fc40f994f7e8c943cceb032822ee1db1f014
mikola-s <suharev.nv@gmail.com>
2019-12-25 12:59:34 +0200

    lsn35 add __str__() in models.py

--- youtube/models.py
+++ youtube/models.py
@@ -4,14 +4,17 @@ from django.db import models
 # Create your models here.
 
 class Gender(models.Model):
-    gender = models.CharField(max_length=64)
+    gender = models.CharField(max_length=64, null=True, blank=True)
 
     def __str__(self):
         return self.gender
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=64)
+    status = models.CharField(max_length=64, null=True, blank=True)
+
+    def __str__(self):
+        return self.status
 
 
 class User(models.Model):
@@ -23,8 +26,8 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     register_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    gender = models.ForeignKey(Gender, on_delete=models.CASCADE)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return f'site user {self.login}'
@@ -33,8 +36,14 @@ class User(models.Model):
 class Channel(models.Model):
     title = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.title
+
 
 class Movie(models.Model):
     title = models.CharField(max_length=255)
     post_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
     file = models.FileField(upload_to='youtube/media/mock_file.jpg')
+
+    def __str__(self):
+        return self.title
