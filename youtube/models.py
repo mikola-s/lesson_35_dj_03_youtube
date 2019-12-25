dd9629db461758fdd3269f89e799b12b902cd56f
mikola-s <suharev.nv@gmail.com>
2019-12-25 13:10:08 +0200

    lsn35 plays requirement parameter

--- youtube/models.py
+++ youtube/models.py
@@ -4,14 +4,14 @@ from django.db import models
 # Create your models here.
 
 class Gender(models.Model):
-    gender = models.CharField(max_length=64, null=True, blank=True)
+    gender = models.CharField(max_length=64)
 
     def __str__(self):
         return self.gender
 
 
 class Status(models.Model):
-    status = models.CharField(max_length=64, null=True, blank=True)
+    status = models.CharField(max_length=64)
 
     def __str__(self):
         return self.status
@@ -26,8 +26,8 @@ class User(models.Model):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=255)
     register_time = models.DateTimeField(auto_now_add=True, null=True, blank=True)
-    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True)
-    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True)
+    gender = models.ForeignKey(Gender, on_delete=models.CASCADE, null=True, blank=True)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return f'site user {self.login}'
