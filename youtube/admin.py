7942e7b3d5696d198ec70f7e4009176ae7c221af
mikola-s <suharev.nv@gmail.com>
2019-12-25 07:38:07 +0200

    lsn35 youtube/admin.py add models

--- youtube/admin.py
+++ youtube/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+
+admin.register(models.User)
+admin.register(models.Channel)
+admin.register(models.Movie)
+admin.register(models.Gender)
+admin.register(models.Status)
