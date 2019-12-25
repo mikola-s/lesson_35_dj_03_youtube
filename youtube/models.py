eda2c738763cee7832bc760a0086bb8ef0df33b0
mikola-s <suharev.nv@gmail.com>
2019-12-25 14:35:25 +0200

    lsn35 youtube/models.py add chanell.owner

--- youtube/models.py
+++ youtube/models.py
@@ -35,6 +35,7 @@ class User(models.Model):
 
 class Channel(models.Model):
     title = models.CharField(max_length=255)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
